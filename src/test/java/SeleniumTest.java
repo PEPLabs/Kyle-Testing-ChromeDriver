3340dd681908799ef02625d1d3b04a747dcb3bbe
Kyle Plummer <kyle.plummer@revature.com>
2025-09-30 10:37:59 -0400

    adding delays

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.time.Duration;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTest {
 
@@ -26,23 +29,28 @@ public class SeleniumTest {
     @Before
     public void setUp() {
 
+        //Testing locally on windows... tests pass.
+//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+
 
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-
         ChromeOptions options = new ChromeOptions();
         options.setBinary("/usr/bin/chromium-browser");
         options.addArguments("--remote-allow-origins=*");
         options.setBinary("/usr/bin/chromium-browser");
         options.addArguments("--remote-allow-origins=*");
-
         // Fix the user data directory issue
         options.addArguments("--user-data-dir=/tmp/chrome-" + System.currentTimeMillis());
-
         // Fix permission/display issues
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");  // Run without GUI
         options.addArguments("--disable-gpu");
+        // Wait up to 10 seconds when looking for any element
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 
         webDriver = new ChromeDriver(options);
 
@@ -56,6 +64,11 @@ public class SeleniumTest {
 
     @Test
     public void testCreateOnBulb() {
+        // Wait up to 10 seconds for the element to be present
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement onBulb = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("onBulb")));
+
+
         // Click the 'on' bulb button
         WebElement onButton = webDriver.findElement(By.id("onBulb"));
         onButton.click();
